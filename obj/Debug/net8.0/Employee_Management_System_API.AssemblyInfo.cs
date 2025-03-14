0e105c0e2f17f0cfdc58c4583435551a3549d313
Nilesh Khairnar <139966220+NKhairnar203@users.noreply.github.com>
2025-03-15 00:32:40 +0530

    add initial

--- obj/Debug/net8.0/Employee_Management_System_API.AssemblyInfo.cs
+++ obj/Debug/net8.0/Employee_Management_System_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Employee_Management_System_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a99b33172a99854186d004d413df97d6bb1ca69")]
 [assembly: System.Reflection.AssemblyProductAttribute("Employee_Management_System_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Employee_Management_System_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
